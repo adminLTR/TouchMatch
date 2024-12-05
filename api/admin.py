52dce8f6a792f2fb32238665347c9b91549bb5b0
Luis LT <105554066+LTprograms@users.noreply.github.com>
2024-12-05 03:18:18 -0500

    modelling and admin panel

---
+++ api/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+@admin.register(ESP32)
+class ESP32Admin(admin.ModelAdmin):
+    list_display = ("id", "user", "code")
+    search_fields = ("code", "user")
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ("id", "code", "datetime_created", "individual","active")
+    list_filter = ("active", "individual")
+    search_fields = ("code",)
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ("id", "room", "level", "datetime_created", "active")
+    list_filter = ("room", "level", "active")
+
+
+@admin.register(UserRegistration)
+class UserRegistrationAdmin(admin.ModelAdmin):
+    list_display = ("id", "get_username", "esp32", "datetime_registration")
+    list_filter = ("room", "level", "active")
+
+    @admin.display(description='Username', ordering='esp32__user__username')
+    def get_username(self, obj):
+        return obj.esp32.user.username
---
+++ api/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class ESP32(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Código")
+    code = models.CharField(max_length=20, unique=True, verbose_name="Código")
+
+    def __str__(self):
+        return f"Placa {self.code}"
+    
+    class Meta:
+        verbose_name = "ESP32"
+        verbose__plural_name = "ESP32"
+
+class Room(models.Model):
+    code = models.CharField(max_length=20, unique=True, verbose_name="Código")
+    datetime_created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
+    individual = models.BooleanField(default=True, verbose_name="Individual")
+    active = models.BooleanField(default=True, verbose_name="Activa")
+
+    def __str__(self):
+        return f"Sala {self.code}"
+    
+    class Meta:
+        verbose_name = "Sala"
+        verbose_plural_name = "Salas"
+
+class Game(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, verbose_name="Sala")
+    level = models.PositiveSmallIntegerField(default=1, verbose_name="Nivel")
+    datetime_created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
+    begin_time = models.DateTimeField(null=True, blank=True, verbose_name="Fecha & Hora de Inicio")
+    end_time = models.DateTimeField(null=True, blank=True, verbose_name="Fecha & Hora de Fiin")
+    active = models.BooleanField(default=False, verbose_name="Activo")
+    sequence = models.CharField(max_length=300, null=True, blank=True, verbose_name="Secuencia")
+
+    def __str__(self):
+        return f"Partida {self.room.code}-{self.pk}"
+    
+    class Meta:
+        verbose_name = "Partida"
+        verbose_plural_name = "Partidas"
+
+class UserRegistration(models.Model):
+    esp32 = models.ForeignKey(ESP32, on_delete=models.CASCADE, verbose_name="Placa")
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, verbose_name="Partida")
+    datetime_registration = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de registro")
+    good_points = models.PositiveSmallIntegerField(default=0, null=True, blank=True, verbose_name="Aciertos")
+    bad_points = models.PositiveSmallIntegerField(default=0, null=True, blank=True, verbose_name="Errores")
+    avg_time_react = models.FloatField(default=0, null=True, blank=True, verbose_name="Tiempo promedio de reacción")
+
+    def __str__(self):
+        return f"{self.esp32.user.username} ({self.esp32}) - {self.game}"
+    
+    class Meta:
+        verbose_name = "Partida de usuario"
+        verbose_plural_name = "Partidas de usuario"
+
