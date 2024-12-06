6aef8e4c6fe85fb48e25761a11f7ccbe7234b8fe
LTprograms <105554066+LTprograms@users.noreply.github.com>
2024-12-06 09:27:09 -0500

    db

---
+++ README.md
@@ -0,0 +1,9 @@
+python -m venv env
+
+./env/Scripts/activate
+
+pip install -r requirements.txt
+
+python manage.py migrate
+
+python manage.py runserver
--- TouchMatch/settings.py
+++ TouchMatch/settings.py
@@ -80,16 +80,23 @@ WSGI_APPLICATION = 'TouchMatch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv("DBNAME"),
+#         'USER': os.getenv("DBUSER"),
+#         'PASSWORD': os.getenv("DBPASSWORD"),
+#         'HOST': os.getenv("DBHOST"),
+#         'PORT': os.getenv("DBPORT"),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DBNAME"),
-        'USER': os.getenv("DBUSER"),
-        'PASSWORD': os.getenv("DBPASSWORD"),
-        'HOST': os.getenv("DBHOST"),
-        'PORT': os.getenv("DBPORT"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
